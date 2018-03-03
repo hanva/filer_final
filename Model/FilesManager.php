dac276e5bcca818b32283ca4e13a7304905d9fe7
Nicolas <nicolas.tran@supinternet.fr>
2018-03-03 13:44:14 +0100

    added title for adding files

--- Model/FilesManager.php
+++ Model/FilesManager.php
@@ -14,18 +14,47 @@ class FilesManager
         $dir = './files/' . $_SESSION['username'] . '/';
         $files = array_diff(scandir($dir), array(".", ".."));
         foreach ($files as $value) {
-            array_push($data, $value);
+
+            if (is_file($dir . $value) === true) {
+                array_push($data, $value);
+            };
         }
         return $data;
     }
-    public function seePath($username)
+    public function seeFilesPaths($username)
     {
         $data = [];
         $dir = './files/' . $_SESSION['username'] . '/';
         $files = array_diff(scandir($dir), array(".", ".."));
         foreach ($files as $value) {
-            array_push($data, $dir . $value);
+            if (is_file($dir . $value) === true) {
+                array_push($data, $dir . $value);
+            }
         }
         return $data;
+    }
+    public function seeFolder($username)
+    {
+        $data = [];
+        $dir = './files/' . $_SESSION['username'] . '/';
+        $files = array_diff(scandir($dir), array(".", ".."));
+        foreach ($files as $value) {
+            if (is_file($dir . $value) === false) {
+                array_push($data, $value);
+            }
+
+        }
+        return $data;
+    }
+    public function addFile($files, $title, $ext)
+    {
+        if ($title !== "") {
+            $files = $title . "." . $ext;
+        }
+        $username = $_SESSION['username'];
+        $uploaddir = './files/' . $username . '/';
+        $uploadfile = $uploaddir . basename($files);
+        move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+        var_dump($_FILES['userfile']['tmp_name']);
     }
 }
